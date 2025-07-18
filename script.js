466547e10d4d0b2007873ad80b97b81177776c9f
godan9370 <dan.go@bythirdparty.com>
2025-07-18 12:21:34 +1000

    Update script.js

--- script.js
+++ script.js
@@ -230,3 +230,6 @@ window.onload = () => {
   if (bgMusic) bgMusic.play();
 };
 
+setInterval(() => {
+  document.body.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+}, 800);
